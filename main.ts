f087c8a7ce4525008bddd306826037430888b3c6
Herri Fransisca <herrifransisca@outlook.com>
2017-08-05 19:22:03 +0700

    Type Assertions.

--- main.ts
+++ main.ts
@@ -1,14 +1,7 @@
+// let message = 'abc';  // type: string
+// let endsWithC = message.endsWith('c');
 
-let a: number;
-let b: boolean;
-let c: string;
-let d: any;
-let e: number[] = [1, 2, 3];
-let f: any[] = [1, true, 'a', false];
-
-const RedColor = 0;
-const GreenColor = 1;
-const BlueColor = 2;
-
-enum Color { Red = 0, Green = 1, Blue = 2, Purple = 3 }
-let backgroundColor = Color.Red;
+let message;    // type: any
+message = 'abc';
+let endsWithC = (<string>message).endsWith('c');
+let alternativeWay = (message as string).endsWith('c');
