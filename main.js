d229e4655edfe4134a43dc1a9ce6d41edd9e7014
Herri Fransisca <herrifransisca@outlook.com>
2017-08-06 22:10:31 +0700

    Properties.

--- main.js
+++ main.js
@@ -1,17 +1,50 @@
+// class Point {
+//   constructor(private x?: number, private y?: number) {
+//   }
+//   draw() {
+//     console.log('X: ' + this.x + ', Y: ' + this.y);
+//   }
+//   getX() {
+//     return this.x;
+//   }
+//   setX(value) {
+//     if (value <= 0)
+//       throw new Error('value cannot be less than 0.');
+//     this.x = value;
+//   }    
+// }
+// let point = new Point(1, 2);
+// let x = point.getX();
+// point.setX(10);
+// point.draw();
+// USING PROPERTY
 var Point = (function () {
-    function Point() {
+    function Point(_x, _y) {
+        this._x = _x;
+        this._y = _y;
     }
     Point.prototype.draw = function () {
-        console.log('X: ' + this.x + ', Y: ' + this.y);
+        console.log('X: ' + this._x + ', Y: ' + this._y);
     };
-    Point.prototype.getDistance = function (another) {
-        //...
-    };
+    Object.defineProperty(Point.prototype, "x", {
+        get: function () {
+            return this._x;
+        },
+        set: function (value) {
+            console.log('value: ' + value);
+            if (value < 0)
+                throw new Error('value cannot be less than 0.');
+            this._x = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
     return Point;
 }());
-// Object
-// let point: Point = new Point();
-var point = new Point(); // shorter way
-point.x = 1;
-point.y = 2;
+var point = new Point(1, 2);
+var x = point.x;
+point.x = 10;
 point.draw();
+// Here's the lesson:
+// a property looks like a field from the outside, but internaly, it's really a method in the class.
+// well, more accurately is in one method which is a getter and a setter or a combination of getter and setter. 
--- main.ts
+++ main.ts
@@ -1,30 +1,64 @@
-// Access modifiers:
-// 1. private, 2. public, 3. protected
-// default = public
+// class Point {
+//   constructor(private x?: number, private y?: number) {
+//   }
 
+//   draw() {
+//     console.log('X: ' + this.x + ', Y: ' + this.y);
+//   }
+
+//   getX() {
+//     return this.x;
+//   }
+
+//   setX(value) {
+//     if (value <= 0)
+//       throw new Error('value cannot be less than 0.');
+
+//     this.x = value;
+//   }    
+// }
+
+// let point = new Point(1, 2);
+// let x = point.getX();
+// point.setX(10);
+// point.draw();
+
+
+
+// USING PROPERTY
 class Point {
-  // # 1 Way 
-  // private x: number;
-  // private y: number;
-
-  // constructor(x?: number, y?: number) {
-  //   this.x = x;
-  //   this.y = y;
-  // }
-
-
-  // # 2 Other and BEST Way and has the exactly the same with #1
-  constructor(private x?: number, private y?: number) {
+  constructor(private _x?: number, private _y?: number) {
   }
 
-
-
   draw() {
-    console.log('X: ' + this.x + ', Y: ' + this.y);
+    console.log('X: ' + this._x + ', Y: ' + this._y);
+  }
+
+  get x() {
+    return this._x;
+  }
+
+  set x(value) {
+    if (value < 0)
+      throw new Error('value cannot be less than 0.');
+
+    this._x = value;
+  }
+
+  setX(value) {
+    this._x = value;
   }
 }
 
 let point = new Point(1, 2);
-// point.x = 3;
-// point.y = 4;
+let x = point.x;
+point.x = 10;
+// point.x(10);   // why cannot write this ? is it the same with point.x = 10 ?
+                  // the answer: because x is KNOWN as a property, not a METHOD
+point.setX(10);   // setX is a METHOD. Kesimpulannya, bila ada kata "SET" / "GET", itu berarti PROPERTY
 point.draw();
+
+
+// Here's the lesson:
+// a property looks like a field from the outside, but internaly, it's really a method in the class.
+// well, more accurately is in one method which is a getter and a setter or a combination of getter and setter.
