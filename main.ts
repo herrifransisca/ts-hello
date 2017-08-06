a158060cc9bb7499b2c0f0bfa93a86430b7586f9
Herri Fransisca <herrifransisca@outlook.com>
2017-08-06 07:27:37 +0700

    Interfaces.

--- main.ts
+++ main.ts
@@ -1,8 +1,45 @@
 
-let log = function (message) {
-  console.log(message);
+// #1
+// let drawPoint = (x, y) => {
+//   //...
+// }
+
+// #2
+// let drawPoint = (x, y, a, b, c, d, e, f) => {
+//   //...
+// }
+
+// #3
+// let drawPoint = (point) => {
+//   //...
+// }
+
+// drawPoint({
+//   x: 1,
+//   y: 2
+// })
+
+// drawPoint({
+//   x: 'Mosh'    // will break the function (expect number for drawing point)
+// })
+
+// #4  // using inline annotation (to solve breaking the function, x: 'Mosh')
+// let drawPoint = (point: { x: number, y: number }) => {
+//   //...
+// }
+
+// drawPoint({
+//   x: 1,
+//   y: 2
+// })
+
+
+#5  // using interface (best way)
+interface Point {
+  x: number,
+  y: number  
 }
 
-//shorter way, using ARROW FUNCTION (Lambda Expression in C#)
-let doLog = (message) => console.log(message);
-
+let drawPoint = (point: Point) => {
+  //...
+}
