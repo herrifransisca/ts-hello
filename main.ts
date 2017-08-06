78789c392df1fbaca05471093689fd30295917f4
Herri Fransisca <herrifransisca@outlook.com>
2017-08-06 18:52:12 +0700

    Constructors.

--- main.ts
+++ main.ts
@@ -1,20 +1,37 @@
 
+// class Point {
+//   x: number;
+//   y: number;
+
+//   constructor(x: number, y: number) {
+//     this.x = x;
+//     this.y = y;
+//   }
+
+//   draw() {
+//     console.log('X: ' + this.x + ', Y: ' + this.y);
+//   }
+// }
+
+// let point = new Point(1, 2);
+// point.draw();
+
+
+// because in typescript, doesn't have "MULTI CONSTRUCTOR" like in C#, use "optional (?)"
+
 class Point {
   x: number;
   y: number;
 
+  constructor(x?: number, y?: number) {
+    this.x = x;
+    this.y = y;
+  }
+
   draw() {
     console.log('X: ' + this.x + ', Y: ' + this.y);
   }
-
-  getDistance(another: Point) {
-    //...
-  }
 }
 
-// Object
-// let point: Point = new Point();
-let point = new Point();    // shorter way
-point.x = 1;
-point.y = 2;
+let point = new Point();
 point.draw();
