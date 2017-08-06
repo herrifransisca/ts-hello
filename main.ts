deb41a5b8764f6b7451a991c9d3164b88e39479c
Herri Fransisca <herrifransisca@outlook.com>
2017-08-06 07:39:10 +0700

    Classes.

--- main.ts
+++ main.ts
@@ -1,45 +1,13 @@
 
-// #1
-// let drawPoint = (x, y) => {
-//   //...
-// }
+class Point {
+  x: number;
+  y: number;
 
-// #2
-// let drawPoint = (x, y, a, b, c, d, e, f) => {
-//   //...
-// }
+  draw() {
+    //...
+  }
 
-// #3
-// let drawPoint = (point) => {
-//   //...
-// }
-
-// drawPoint({
-//   x: 1,
-//   y: 2
-// })
-
-// drawPoint({
-//   x: 'Mosh'    // will break the function (expect number for drawing point)
-// })
-
-// #4  // using inline annotation (to solve breaking the function, x: 'Mosh')
-// let drawPoint = (point: { x: number, y: number }) => {
-//   //...
-// }
-
-// drawPoint({
-//   x: 1,
-//   y: 2
-// })
-
-
-#5  // using interface (best way)
-interface Point {
-  x: number,
-  y: number  
-}
-
-let drawPoint = (point: Point) => {
-  //...
+  getDistance(another: Point) {
+    //...
+  }
 }
