f8f0d498e03b9717b2356079db3760a99d733ad4
Herri Fransisca <herrifransisca@outlook.com>
2017-08-06 21:35:30 +0700

    Access Modifiers in Constructor Parameters.

--- main.ts
+++ main.ts
@@ -3,13 +3,21 @@
 // default = public
 
 class Point {
-  private x: number;
-  private y: number;
+  // # 1 Way 
+  // private x: number;
+  // private y: number;
 
-  constructor(x?: number, y?: number) {
-    this.x = x;
-    this.y = y;
+  // constructor(x?: number, y?: number) {
+  //   this.x = x;
+  //   this.y = y;
+  // }
+
+
+  // # 2 Other and BEST Way and has the exactly the same with #1
+  constructor(private x?: number, private y?: number) {
   }
+
+
 
   draw() {
     console.log('X: ' + this.x + ', Y: ' + this.y);
