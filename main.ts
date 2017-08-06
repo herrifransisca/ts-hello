749cf28e131c030c6d869fee4f4044d9c90e67df
Herri Fransisca <herrifransisca@outlook.com>
2017-08-06 21:30:45 +0700

    Access Modifiers.

--- main.ts
+++ main.ts
@@ -1,27 +1,10 @@
-
-// class Point {
-//   x: number;
-//   y: number;
-
-//   constructor(x: number, y: number) {
-//     this.x = x;
-//     this.y = y;
-//   }
-
-//   draw() {
-//     console.log('X: ' + this.x + ', Y: ' + this.y);
-//   }
-// }
-
-// let point = new Point(1, 2);
-// point.draw();
-
-
-// because in typescript, doesn't have "MULTI CONSTRUCTOR" like in C#, use "optional (?)"
+// Access modifiers:
+// 1. private, 2. public, 3. protected
+// default = public
 
 class Point {
-  x: number;
-  y: number;
+  private x: number;
+  private y: number;
 
   constructor(x?: number, y?: number) {
     this.x = x;
@@ -33,5 +16,7 @@ class Point {
   }
 }
 
-let point = new Point();
+let point = new Point(1, 2);
+// point.x = 3;
+// point.y = 4;
 point.draw();
